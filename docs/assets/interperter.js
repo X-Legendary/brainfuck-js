00bf6b213d61010445aad017cee600dde15e7018
X-Legendary <81035170+X-Legendary@users.noreply.github.com>
2022-02-24 13:36:13 +0100

    Added support for loops

--- docs/assets/interperter.js
+++ docs/assets/interperter.js
@@ -4,13 +4,27 @@ const inputArea = document.getElementById("input")
 const outputArea = document.getElementById("output")
 const cellNumber = document.getElementById("numcells")
 
-
+// Code works if you run it in Node.js but doesn't in the browser?
 const interpret = code => {
     let pointer = 0
     let cells = new Array(cellNumber || 64).fill(0)
     let output = ""
+    let loopNum = 0
+    let loop  = false
+    let loopArray  = []
+
     for (let i = 0; i < code.length; i++) {
         const cmd = code[i]
+
+        if(loop) {
+            if(char === "[") loopNum++
+            if(char === "]") {
+              if(loopNum === 0) looping = false
+              else loopNum--
+            }
+            continue
+        }
+
         switch (cmd) {
             case ">":
                 if(pointer == cells.length - 1) {
@@ -42,6 +56,14 @@ const interpret = code => {
                 } else {
                     cells[pointer] = input.charCodeAt(0)
                 }
+            case "[":
+                if(cells[pointer] == 0) looping = true
+                else loopArray.push(i)
+            break;
+            case "]":
+                if(cells[pointer] != 0) i = loopArray[loopArray.length - 1]
+                else loopArray.pop()
+            break;
             default:
                 break;
         }
